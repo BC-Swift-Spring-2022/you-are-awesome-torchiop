2746f631b146e27f5c569a886623826abb00cb30
Peter Torchio <torchiop@bc.edu>
2022-01-30 20:16:23 -0500

    2.6 Work -- Added image, activated by show message button

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -8,19 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("👍 viewDidLoad has run!")
-        messageLabel.text = "Fabulous? That's You!"
     }
 
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        print("😎 The message button was pressed!")
-        
         messageLabel.text = "You Are Awesome!"
+        imageView.image = UIImage(named: "A45D66DF-D52B-4240-8352-1F1238832164 copy")
     }
     
 }
